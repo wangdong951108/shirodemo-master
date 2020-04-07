5567dc941d1e695336e4cb8dc0acb0e4725ce118
wd1108 <17614744821@163.com>
2020-04-07 10:05:30 +0800

    修改github登录名，测试提交2

--- shirodemo/src/main/java/com/shiro/shirodemo/api/test002Api.java
+++
@@ -1,8 +0,0 @@
-package com.shiro.shirodemo.api;
-
-/**
- * 描述：
- * Created by wangdong on 2020-04-07 9:36
- */
-public class test002Api {
-}
--- shirodemo/src/main/java/com/shiro/shirodemo/api/test003Api.java
+++
@@ -1,8 +0,0 @@
-package com.shiro.shirodemo.api;
-
-/**
- * 描述：
- * Created by wangdong on 2020-04-07 10:01
- */
-public class test003Api {
-}
